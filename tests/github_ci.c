0a3cfe1a0daac938b835c49f7c85909d1cd58e3a
Alex Evelyn <gpg@senorcontento.com>
2019-11-24 01:36:55 -0500

    Test Sending To MQTT (this case Netcat) Server

--- tests/github_ci.c
+++ tests/github_ci.c
@@ -39,7 +39,7 @@ static struct ev_timer reconnect_timer;
 // NOTE: For encryption, specify -s on the commandline (e.g. ./example/example -s)
 static struct config cfg = {
     .host = "localhost",
-    .port = 8884,
+    .port = 8883,
     .options = {
         .keep_alive = 30,
         .clean_session = true,
